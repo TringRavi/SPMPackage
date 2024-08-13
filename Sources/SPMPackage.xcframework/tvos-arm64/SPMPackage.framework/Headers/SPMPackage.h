b99865a63b934918194efda5feaab1c5d4c65c5a
Ravi Chandran <ravi.chandran@tringapps.com>
2024-08-13 18:40:34 +0530

    FirstCommit

---
+++ Sources/SPMPackage.xcframework/tvos-arm64/SPMPackage.framework/Headers/SPMPackage.h
@@ -0,0 +1,18 @@
+//
+//  SPMPackage.h
+//  SPMPackage
+//
+//  Created by Ravi Chandran on 13/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMPackage.
+FOUNDATION_EXPORT double SPMPackageVersionNumber;
+
+//! Project version string for SPMPackage.
+FOUNDATION_EXPORT const unsigned char SPMPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMPackage/PublicHeader.h>
+
+
